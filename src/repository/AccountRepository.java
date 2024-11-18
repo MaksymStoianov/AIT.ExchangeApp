848effb6ba7db765900ef82d9010e579d0e3cded
IgorDerk <derk.i26252@gmail.com>
2024-11-18 13:00:30 +0100

    echange Account

--- src/repository/AccountRepository.java
+++ src/repository/AccountRepository.java
@@ -2,64 +2,78 @@ package repository;
 
 import model.Account;
 import model.Transaction;
+import model.User;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public interface AccountRepository {
-  Map<LocalDateTime, Transaction> transactionsHistory = new LinkedHashMap<>();
+    /**
+     * Список пользователей
+     * key - email user
+     * value - список счетов пользователя
+     */
+    Map<String, List<Account>> users = new LinkedHashMap<>();
+
+    /**
+     * Список транзакций по счету
+     * key - ID счета
+     * value - список транзакций по счету
+     */
+    Map<Integer, List<Transaction>> transactions = new LinkedHashMap<>();
 
 
-  /**
-   * Добавляет счет к пользователю в определенной валюте.
-   *
-   * @param title    Название счета.
-   * @param currency Название валюты.
-   * @return Счет.
-   */
-  Account creatAccount(String title, String currency);
+    /**
+     * Добавляет счет к пользователю в определенной валюте.
+     *
+     * @param title    Название счета.
+     * @param currency Название валюты.
+     * @return Счет.
+     */
+    Account creatAccount(String title, String currency);
 
 
-  /**
-   * Возвращает список всех счетов пользователя.
-   *
-   * @return Список всех счетов пользователя.
-   */
-  List<Account> getAllAccounts();
+    /**
+     * Возвращает список всех счетов пользователя.
+     *
+     * @return Список всех счетов пользователя.
+     */
+    List<Account> getAllAccounts();
 
 
-  /**
-   * Возвращает счет пользователя по его уникальному идентификатору.
-   *
-   * @return Счет.
-   */
-  Account getAccountById(int id);
+    /**
+     * Возвращает счет пользователя по его уникальному идентификатору.
+     *
+     * @return Счет.
+     */
+    Account getAccountById(int id);
 
 
-  /**
-   * Возвращает список счетов по коду валюты.
-   *
-   * @param currency
-   * @return
-   */
-  List<Account> getAccountsByCurrency(String currency);
+    /**
+     * Возвращает список счетов по коду валюты.
+     *
+     * @param currency
+     * @return
+     */
+    List<Account> getAccountsByCurrency(String currency);
 
 
-  /**
-   * Удаляет счет из списка счетов пользователя.
-   *
-   * @param id Уникальный идентификатор счета.
-   */
-  void removeAccount(int id);
+    /**
+     * Удаляет счет из списка счетов пользователя.
+     *
+     * @param id Уникальный идентификатор счета.
+     */
+    void removeAccount(int id);
 
 
-  /**
-   * Удаляет счет из списка счетов пользователя.
-   *
-   * @param account Счет.
-   */
-  void removeAccount(Account account);
+    /**
+     * Удаляет счет из списка счетов пользователя.
+     *
+     * @param account Счет.
+     */
+    void removeAccount(Account account);
 
 }
