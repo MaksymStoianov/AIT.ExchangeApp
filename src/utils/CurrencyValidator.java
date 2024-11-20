e7119ab0064cc38985965b313bd84aa82e39a868
Ang <zajkopus@gmail.com>
2024-11-20 12:28:05 +0100

    space

---
+++ src/utils/CurrencyValidator.java
@@ -0,0 +1,21 @@
+package utils;
+
+import model.CurrencyCode;
+import utils.exceptions.CurrencyCodeValidateExeption;
+
+public class CurrencyValidator {
+    public static boolean isValidCurrencyCode(String currencyCode){
+        boolean currencyValid = false;
+        for (CurrencyCode code : CurrencyCode.values()){
+            if (code.name().equalsIgnoreCase(currencyCode)) {
+                currencyValid = true;
+            }
+        }
+        if (currencyValid == false) {
+            String errorMessage = "Введённый код валюты " + currencyCode + " не находится в системе";
+            throw new CurrencyCodeValidateExeption(errorMessage);
+        }
+
+        return currencyValid;
+    }
+}
